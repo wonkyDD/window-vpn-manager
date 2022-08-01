6cd7a239931fbbf8c70e420234e65b926efa5ad2
wonkyDD <dev.csh.01@gmail.com>
2022-08-01 13:33:08 +0900

    :hammer: Add **Mullvad** related batchfiles

---
+++ script/mullvad_off.bat
@@ -0,0 +1,45 @@
+@echo off
+echo.
+echo.
+
+for /f "delims=" %%i in ('mullvad account get') do (
+    echo %%i
+) >> temp.txt
+
+python ..\src\expire.py
+
+echo.
+echo.
+mullvad always-require-vpn set off
+mullvad disconnect
+echo.
+echo.
+
+:begin
+for /f "delims=" %%i in ('mullvad status') do set output=%%i
+if %output%==Disconnected goto end
+echo Not Yet..
+goto begin
+
+:end
+echo %output%
+
+echo.
+echo.
+@REM Stop-Process -Name chrome
+@REM 안전한 수단인 걸까? 위에 놈은 powershell에서는 되는데 batch에선 또 안됨
+
+
+taskkill /F /IM chrome.exe /T
+
+@REM %userprofile%/AppData/Local/Google/Chrome/Default/UserData/에서
+@REM History 또는 History-journal을 주목.. Auto Wipe가 작동 안하는 이유?
+
+@REM powershell에서는 되는데 batchfile에서는 안됨
+@REM Set-Alias chrome "C:\Program Files\Google\Chrome\Application\chrome.exe"
+@REM chrome --headless --print-to-pdf=d:\naver.pdf http://naver.com
+
+python ..\src\end.py
+
+echo.
+echo.
---
+++ script/mullvad_on.bat
@@ -0,0 +1,47 @@
+@echo off
+echo.
+echo.
+
+for /f "delims=" %%i in ('mullvad account get') do (
+    echo %%i
+) >> temp.txt
+
+python ..\src\expire.py
+
+echo.
+echo.
+mullvad always-require-vpn set on
+mullvad connect
+
+
+:begin
+for /f "delims=" %%i in ('mullvad status') do set output=%%i
+
+for %%i in (%output%) do (
+    if %%i==Connected goto end
+
+    echo Not Yet..
+    goto begin
+) 
+
+:end
+echo %output%
+
+
+@REM start chrome 무조건 앞에 위치해야함
+python ..\src\start.py
+
+
+start chrome https://kissjav.li/videos/china-porn/ 
+start chrome https://kissjav.li/videos/asian-porn-movies/ 
+start chrome https://kissjav.li/videos/korean-bj/ 
+start chrome https://kissjav.li/videos/korean-porn/ 
+@REM start chrome chrome://downloads/
+
+
+
+
+echo.
+echo.
+@REM start.py의 fetch가 담당함
+@REM for /f "tokens=*" %%s in (..\remember.txt) do echo %%s
