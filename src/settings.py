7c83191978eda62c1af591d29ba8672d5fdb7b6f
wonkyDD <dev.csh.01@gmail.com>
2022-08-03 02:11:31 +0900

    :rotating_light: Fix `pylint` and `mypy` warnings

--- src/settings.py
+++ src/settings.py
@@ -1,47 +1,47 @@
 from pathlib import Path
 
-WEEKDAYS = ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun']
+WEEKDAYS = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"]
 
 DRIVER_TYPES = {
-    'firefox'   : 'Firefox',
-    'chrome'    : 'Chrome',
+    "firefox": "Firefox",
+    "chrome": "Chrome",
 }
 
 TARGET_LINKS = {
-    'korean-porn'           : 'https://kissjav.li/videos/korean-porn/',
-    'korean-bj'             : 'https://kissjav.li/videos/korean-bj/',
-    'asian-porn-movies'     : 'https://kissjav.li/videos/asian-porn-movies/',
-    'china-porn'            : 'https://kissjav.li/videos/china-porn/',
+    "korean-porn": "https://kissjav.li/videos/korean-porn/",
+    "korean-bj": "https://kissjav.li/videos/korean-bj/",
+    "asian-porn-movies": "https://kissjav.li/videos/asian-porn-movies/",
+    "china-porn": "https://kissjav.li/videos/china-porn/",
 }
 
 SCREENSHOT_NAMES = {
-    'korean-porn' : 'korean-porn.png',
-    'korean-bj' : 'korean-bj.png',
-    'asian-porn-movies' : 'asian-porn-movies.png',
-    'china-porn' : 'china-porn.png'
+    "korean-porn": "korean-porn.png",
+    "korean-bj": "korean-bj.png",
+    "asian-porn-movies": "asian-porn-movies.png",
+    "china-porn": "china-porn.png",
 }
-SCREENSHOT_PATH = str(Path(__file__).parent.parent) + '\\materials\\screenshots\\'
+SCREENSHOT_PATH = str(Path(__file__).parent.parent) + "\\materials\\screenshots\\"
 
 
-SRC         = 'C:\\Users\\USER\\Downloads\\'
-KOR_DEST    = 'Z:\\Korea\\kissjav\\'
-CHN_DEST    = 'Z:\\.CHINA\\'
-MOV_DEST    = 'Z:\\.MOVIE\\'
+SRC = "C:\\Users\\USER\\Downloads\\"
+KOR_DEST = "Z:\\Korea\\kissjav\\"
+CHN_DEST = "Z:\\.CHINA\\"
+MOV_DEST = "Z:\\.MOVIE\\"
 
-DB_NAME     = 'ydmanager.db'
-DB_PATH = str(Path(__file__).parent.parent) + '\\db\\'
-BACKUP_SQL_NAME = 'ydmanager.sql'
-BACKUP_SQL_PATH = str(Path(__file__).parent.parent.parent.parent) + '\\__BACKUP__\\'
+DB_NAME = "ydmanager.db"
+DB_PATH = str(Path(__file__).parent.parent) + "\\db\\"
+BACKUP_SQL_NAME = "ydmanager.sql"
+BACKUP_SQL_PATH = str(Path(__file__).parent.parent.parent.parent) + "\\__BACKUP__\\"
 
 DB_TABLE_NAMES = [
-    'videoId',
+    "videoId",
 ]
 
-TXT_NAME    = 'ydmanager.txt'
-TXT_PATH    = str(Path(__file__).parent.parent) + '\\'
+TXT_NAME = "ydmanager.txt"
+TXT_PATH = str(Path(__file__).parent.parent) + "\\"
 
-LOG_NAME = 'ydmanager.log'
-LOG_PATH = str(Path(__file__).parent.parent) + '\\log\\'
+LOG_NAME = "ydmanager.log"
+LOG_PATH = str(Path(__file__).parent.parent) + "\\log\\"
 
 
 # ACTIONS = []
